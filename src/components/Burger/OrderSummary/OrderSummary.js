26f09be71f71f1cba99ccbf5bb2534b0aa3ab86e
mattericbrown <matt.eric.brown@gmail.com>
2019-04-11 12:24:54 -0700

    mobile styling for sideDrawer and responsive changes

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -7,12 +7,21 @@ const orderSummary = (props) => {
         .map(igKey => {
             return <li key={igKey}>{igKey}: {props.ingredients[igKey]}</li>
         })
+
+    const dollarStyle = {
+        fontSize: '15px',
+        display: 'inline',
+        position: 'relative',
+        bottom: '20px'
+    }
+
     return (
         <Fragment>
             <h3>Your Order</h3>
             <ul>
                 {ingredientSummary}
             </ul>
+            <p>Total Price: <div style={dollarStyle}>$</div>{props.price.toFixed(2)} </p>
             <p>Continue to Checkout?</p>
             <Button btnType="Danger" clicked={props.purchaseCancelled}>Cancel Order</Button>
             <Button btnType="Success" clicked={props.purchaseContinued}>Continue</Button>
